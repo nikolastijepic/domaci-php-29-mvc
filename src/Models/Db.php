d6b9cc0958c71349a9e0325e30e9fedf64b6b972
nikolastijepic <nikola.stijepic94@icloud.com>
2026-06-01 15:50:17 +0200

    Code formatting

--- src/Models/Db.php
+++ src/Models/Db.php
@@ -4,9 +4,9 @@ namespace PHP28\Models;
 
 class Db
 {
-public $connection;
+    public $connection;
 
-public function __construct(){
-    $this->connection = new \PDO("mysql:host=localhost;dbname=php27", "root", "ServBay.dev");
-}
+    public function __construct(){
+        $this->connection = new \PDO("mysql:host=localhost;dbname=php27", "root", "ServBay.dev");
+    }
 }
